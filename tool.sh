98f4f0d6a32e6bbd95490abd4e635d1283a622c6
daniel <danielzi@qq.com>
2021-10-25 14:10:36 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -31,6 +31,13 @@ function skyBoxArm(){
 	wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/armbox.sh && chmod +x box.sh && clear && ./box.sh
 }
 
+#获取本机IP
+function getip(){
+	echo  
+	curl ip.p3terx.com
+	echo
+}
+
 #XrayR 后端
 function xrayR(){
 	apt-get install git -y
@@ -49,6 +56,72 @@ function dockerCompose(){
 	chmod +x /usr/local/bin/docker-compose
 }
 
+#BBR一键管理脚本
+function tcpsh(){
+	wget -O "/root/tcp.sh" "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/tcp.sh" --no-check-certificate -T 30 -t 5 -d
+	chmod +x "/root/tcp.sh"
+	chmod 777 "/root/tcp.sh"
+	blue "下载完成"
+	blue "你也可以输入 bash /root/tcp.sh 来手动运行"
+	bash "/root/tcp.sh"
+}
+
+#ChangeSource Linux换源脚本·下载
+function cssh(){
+	wget -O "/root/changesource.sh" "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/changesource.sh" --no-check-certificate -T 30 -t 5 -d
+	chmod +x "/root/changesource.sh"
+	chmod 777 "/root/changesource.sh"
+	blue "下载完成"
+	echo
+	green "请自行输入下面命令切换对应源"
+	green " =================================================="
+	echo
+	green " bash changesource.sh 切换推荐源 "
+	green " bash changesource.sh cn  切换中科大源 "
+	green " bash changesource.sh aliyun 切换阿里源 "
+	green " bash changesource.sh 163 切换网易源 "
+	green " bash changesource.sh aws 切换AWS亚马逊云源 "
+	green " bash changesource.sh restore 还原默认源 "
+}
+
+#Lemonbench 综合测试
+function Lemonbench(){
+curl -fsL https://ilemonra.in/LemonBenchIntl | bash -s fast
+}
+
+#三网Speedtest测速
+function 3speed(){
+bash <(curl -Lso- https://raw.githubusercontent.com/BlueSkyXN/SpeedTestCN/main/superspeed.sh)
+}
+
+#Superbench 综合测试
+function superbench(){
+wget -O "/root/superbench.sh" "https://raw.githubusercontent.com/BlueSkyXN/ChangeSource/master/superbench.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/superbench.sh"
+chmod 777 "/root/superbench.sh"
+blue "下载完成"
+bash "/root/superbench.sh"
+}
+
+#MTP&TLS 一键脚本
+function mtp(){
+wget -O "/root/mtp.sh" "https://raw.githubusercontent.com/sunpma/mtp/master/mtproxy.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/mtp.sh"
+chmod 777 "/root/mtp.sh"
+blue "你也可以输入 bash /root/mtp.sh 来手动运行"
+blue "下载完成"
+bash "/root/mtp.sh"
+}
+
+#NEZHA.SH哪吒面板/探针·下载
+function nezha(){
+wget -O "/root/nezha.sh" "https://raw.githubusercontent.com/BlueSkyXN/nezha/master/script/install.sh" --no-check-certificate -T 30 -t 5 -d
+chmod +x "/root/nezha.sh"
+chmod 777 "/root/nezha.sh"
+blue "你也可以输入 bash /root/nezha.sh 来手动运行"
+blue "下载完成"
+bash "/root/nezha.sh"
+}
 
 #主菜单
 function start_menu(){
@@ -63,10 +136,16 @@ function start_menu(){
 	green " 3. SKY-BOX ARM工具箱 "
 	
 	yellow " ----------------------------------------------- "
-	green " 4. XrayR 后端 "
-	green " 5. Netflix 检测 sjlleo版 "
-	green " 6. Docker compose 安装 "
-   
+	green " 10. 获取本机IP "
+	green " 11. Netflix 检测 sjlleo版 "
+
+   	yellow " ----------------------------------------------- "
+	green " 20. Docker compose 安装 "
+	
+	yellow " ----------------------------------------------- "
+	green " 30.BBR一键管理脚本 "
+	green " 31. XrayR 后端 "
+	
 	yellow " =============================================== "
 	green " 0. 退出脚本"
     echo
